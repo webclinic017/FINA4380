1ca4ad9fe3a4c6e1efb584c2e32f08fb7432bfdf
omnikdy <k1053909669@gmail.com>
2022-11-24 13:13:21 +0800

    update for iterate

--- kalman.py
+++ kalman.py
@@ -9,7 +9,7 @@ from scipy.stats import pearsonr
 from statsmodels.tsa.stattools import adfuller
 
 def tls(X,y):
-    if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series) :
+    if isinstance(X, np.ndarray)==False:
         X = np.array(X)
         y = np.array(y)
     # form cumulative return
@@ -28,18 +28,17 @@ def tls(X,y):
 class Kalman_filter():
     def __init__(self,data1,data2):
         # use x to predict y
+        # the ini time intervel related to the time length of ini data
+        self.windows = len(self.x)
         self.x = data1
         self.y = data2
         residual,b0,b1= tls(data2,data1)
+        # use the result from tls as observation_matrices
         self.kf = KalmanFilter(initial_state_mean=np.mean(data2), 
                   transition_matrices = 1,observation_matrices = b1,
                   transition_offsets = 0,observation_offsets = b0,
                   n_dim_obs=1,n_dim_state=1)
-        
-        
-    def get_prediction(self):
-        #measurements = self.x
-        n_timesteps = len(self.x)
+        n_timesteps = self.windows
         n_dim_state = 1
         filtered_state_means = np.zeros((n_timesteps, n_dim_state))
         filtered_state_covariances = np.zeros((n_timesteps, n_dim_state, n_dim_state))
@@ -55,14 +54,30 @@ class Kalman_filter():
                     self.x[t + 1]
                 )
             )
-
-        # draw estimates
-        plt.figure()
-        lines_true = plt.plot(self.y, color='b')
-        lines_filt = plt.plot(filtered_state_means, color='r')
-        plt.legend((lines_true[0], lines_filt[0]), ('true', 'filtered'))
-        plt.show()
+        self.previous_cov = filtered_state_covariances[-1]
+        self.state = list(self.y)
+        self.observe = list(self.x)
         
-        return filtered_state_means
+        
+    def iter(self,d1,d2):
+        # update the state
+        self.state.append(d2)
+        self.observe.append(d1)
+        # use window para to update the transition para
+        # use -1 to avoid information leak
+        residual,b0,b1= tls(self.state[-self.windows:-1],self.observe[-self.windows:-1])
+        prediction, pred_covariances = (
+        self.kf.filter_update(
+                    np.mean(self.state[-self.windows:-1]),# use -1 to avoid information leak
+                    self.previous_cov,
+                    d1,
+                    observation_matrix = b1,
+                    observation_offset = b0
+                )
+            )
+        self.previous_cov = pred_covariances
+        pred_error = d2 - prediction
+        
+        return prediction,pred_error
 
 
